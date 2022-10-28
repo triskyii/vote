7e9d41d6bccb93f749cc8db60c0f1fdbb1ba2ad6
triskyii <113702870+triskyii@users.noreply.github.com>
2022-10-28 14:29:24 +0100

    Add files via upload

---
+++ vote.js
@@ -0,0 +1,35 @@
+
+
+
+
+var a1 = 0;
+function but() {
+      a1 = a1 + 1;
+      document.getElementById("a1").innerHTML = a1;
+};
+
+
+
+var a2 = 0;
+function butt() {
+      a2 = a2 + 1;
+      document.getElementById("a2").innerHTML = a2;
+};
+
+
+var a3 = 0;
+function buto() {
+      a3 = a3 + 1;
+      document.getElementById("a3").innerHTML = a3;
+};
+
+
+
+
+
+var a4 = 0;
+function butto() {
+      a4 = a4 + 1;
+      document.getElementById("a4").innerHTML = a4;
+};
+
